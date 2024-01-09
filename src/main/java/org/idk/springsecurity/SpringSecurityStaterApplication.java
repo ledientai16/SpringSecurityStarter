5df5660413e50ed5314e16dbed644736eacb66e3
LDT <ledientai16@gmail.com>
2024-01-09 21:13:09 +0700

    init project

---
+++ src/main/java/org/idk/springsecurity/SpringSecurityStaterApplication.java
@@ -0,0 +1,13 @@
+package org.idk.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityStaterApplication.class, args);
+    }
+
+}
